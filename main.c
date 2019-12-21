91260189b68fe2af5b6250cc40441c47d28ea5ac
raphyuki <raph.yuki@gmail.com>
2019-12-20 22:42:11 -0200

    added undo functionality and fixed score display

--- main.c
+++ main.c
@@ -15,6 +15,16 @@ typedef struct {
   int col_index;
 } zero_pos;
 
+typedef struct s_el {
+  char elem;
+  struct s_el *prev;
+} s_elem;
+
+typedef struct {
+  int size;
+  s_elem *top;
+} stack;
+
 /* Puzzle setup functions */
 
 void reset_array(number_array *array, int diff){
@@ -65,7 +75,43 @@ void find_zero(zero_pos *position, int grid[GRID_SIZE+1][GRID_SIZE+1], int diff)
 
 /* Gameplay functions */
 
-int move_zero(zero_pos *position, int grid[GRID_SIZE+1][GRID_SIZE+1], char input, int diff){
+void create_stack(stack *stk){
+  stk->size = 0;
+  stk->top = NULL;
+}
+
+void store_stack(stack *stk, char move){
+  s_elem *new_elem = malloc(sizeof(s_elem));
+  new_elem->elem = move;
+  new_elem->prev = stk->top;
+  stk->top = new_elem;
+  stk->size++;
+}
+
+char take_stack(stack *stk){
+  if(stk->size==0){
+    return 'F';
+  }
+  s_elem *aux = stk->top;
+  char move = aux->elem;
+  stk->top = aux->prev;
+  free(aux);
+  stk->size--;
+  return move;
+}
+
+void clear_stack(stack *stk){
+  s_elem *aux;
+  while(stk->size>0){
+    aux = stk->top;
+    stk->top = aux->prev;
+    free(aux);
+    stk->size--;
+  }
+}
+
+//would be nice to re-write movements in their own functions
+int move_zero(zero_pos *position, int grid[GRID_SIZE+1][GRID_SIZE+1], char input, int diff, stack *stk){
   //for forced reset
   if(input=='R' || input=='r'){
     printf("Are you sure you want to start a new game? You will lose this game's progress.\nPress Y for yes or any letter for no.\n");
@@ -77,10 +123,37 @@ int move_zero(zero_pos *position, int grid[GRID_SIZE+1][GRID_SIZE+1], char input
       printf("Your game has been resumed. Please enter your next movement.\n");
       return 0;
     }
+  }else if(input=='B' || input=='b'){
+    char move = take_stack(stk);
+    if(move=='F'){
+      printf("There are no movements left to undo.\n");
+      return 0;
+    }else if(move=='W' || move=='w'){
+      int aux = grid[position->row_index][position->col_index];
+      grid[position->row_index][position->col_index] = grid[position->row_index+1][position->col_index];
+      grid[position->row_index+1][position->col_index] = aux;
+      position->row_index++;
+      return 1;
+    }else if(move=='A' || move=='a'){
+      int aux = grid[position->row_index][position->col_index];
+      grid[position->row_index][position->col_index] = grid[position->row_index][position->col_index+1];
+      grid[position->row_index][position->col_index+1] = aux;
+      position->col_index++;
+      return 1;
+    }else if(move=='S' || move=='s'){
+      int aux = grid[position->row_index][position->col_index];
+      grid[position->row_index][position->col_index] = grid[position->row_index-1][position->col_index];
+      grid[position->row_index-1][position->col_index] = aux;
+      position->row_index--;
+      return 1;
+    }else if(move=='D' || move=='d'){
+      int aux = grid[position->row_index][position->col_index];
+      grid[position->row_index][position->col_index] = grid[position->row_index][position->col_index-1];
+      grid[position->row_index][position->col_index-1] = aux;
+      position->col_index--;
+      return 1;
+    }//an else could be added here in case of an unexpected exception
   //for invalid letters
-  }else if(input!='W' && input!='A' && input!='S' && input!='D' && input!='w' && input!='a' && input!='s' && input!='d'){
-    printf("Please enter a valid input.\n");
-    return 0;
   }else if(input=='W' || input=='w'){
     //for border movements
     if(position->row_index==0){
@@ -88,6 +161,7 @@ int move_zero(zero_pos *position, int grid[GRID_SIZE+1][GRID_SIZE+1], char input
       return 0;
     //for valid movements
     }else{
+      store_stack(stk, input);
       int aux = grid[position->row_index][position->col_index];
       grid[position->row_index][position->col_index] = grid[position->row_index-1][position->col_index];
       grid[position->row_index-1][position->col_index] = aux;
@@ -99,6 +173,7 @@ int move_zero(zero_pos *position, int grid[GRID_SIZE+1][GRID_SIZE+1], char input
       printf("Please enter a valid movement.\n");
       return 0;
     }else{
+      store_stack(stk, input);
       int aux = grid[position->row_index][position->col_index];
       grid[position->row_index][position->col_index] = grid[position->row_index][position->col_index-1];
       grid[position->row_index][position->col_index-1] = aux;
@@ -110,6 +185,7 @@ int move_zero(zero_pos *position, int grid[GRID_SIZE+1][GRID_SIZE+1], char input
       printf("Please enter a valid movement.\n");
       return 0;
     }else{
+      store_stack(stk, input);
       int aux = grid[position->row_index][position->col_index];
       grid[position->row_index][position->col_index] = grid[position->row_index+1][position->col_index];
       grid[position->row_index+1][position->col_index] = aux;
@@ -121,17 +197,22 @@ int move_zero(zero_pos *position, int grid[GRID_SIZE+1][GRID_SIZE+1], char input
       printf("Please enter a valid movement.\n");
       return 0;
     }else{
+      store_stack(stk, input);
       int aux = grid[position->row_index][position->col_index];
       grid[position->row_index][position->col_index] = grid[position->row_index][position->col_index+1];
       grid[position->row_index][position->col_index+1] = aux;
       position->col_index++;
       return 1;
     }
+  }else{
+    printf("Please enter a valid input.\n");
+    return 0;
   }
 }
 
-int reset_game(char confirmation){
+int reset_game(char confirmation, stack *stk){
   if(confirmation=='R' || confirmation=='r'){
+    clear_stack(stk);
     return 1;
   }else{
     printf("Press R and Enter to restart the game.\n");
@@ -188,7 +269,7 @@ void show_grid(int grid[GRID_SIZE+1][GRID_SIZE+1], int diff){
 }
 
 void display_instructions(){
-  printf("The objetive of this game is to line the numbers in growing order,\nfrom left to right, then from top to bottom. The only number you\nare allowed to move is the 00, swapping its position with one of its\nadjacent numbers. To move it, you must type the direction you want\nit to move, then press enter.\n\nW - up, A - left, S - down, D - right\n\n");
+  printf("The objetive of this game is to line the numbers in growing order,\nfrom left to right, then from top to bottom. The only number you\nare allowed to move is the 00, swapping its position with one of its\nadjacent numbers. To move it, you must type the direction you want\nit to move, then press enter.\n\nW - up, A - left, S - down, D - right\nR - re-shuffle/new game, B - back/undo\n\n");
 }
 
 void display_diff_selector(){
@@ -208,7 +289,8 @@ void display_score(int score){
 int main(){
   number_array array;
   zero_pos position;
-  int score=1, diff, swap_check = 0, grid[GRID_SIZE+1][GRID_SIZE+1];
+  stack stk;
+  int score=0, diff, swap_check = 0, grid[GRID_SIZE+1][GRID_SIZE+1];
   char input, reset;
 
   display_instructions();
@@ -224,13 +306,14 @@ int main(){
     scramble_array(&array, diff);
     start_grid(array, grid, diff);
     find_zero(&position, grid, diff);
+    create_stack(&stk);
 
     while(check_victory(grid, diff, &score)!=1 && swap_check!=2){
       show_grid(grid, diff);
       swap_check = 0;
       while(swap_check==0){
         scanf("\n%c", &input);
-        swap_check = move_zero(&position, grid, input, diff);
+        swap_check = move_zero(&position, grid, input, diff, &stk);
       }
     }
 
@@ -240,7 +323,7 @@ int main(){
       swap_check = 0;
       reset = 'R';
     }
-    while(reset_game(reset)!=1){
+    while(reset_game(reset, &stk)!=1){
       scanf("\n%c", &reset);
     }
   }
